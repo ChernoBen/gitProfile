b54732921edbfbbf96a0310b8767f0d8ffd4a1f3
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-17 19:32:18 -0300

    github search tool

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,106 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="columns is-mobile is-centered">
+      <div class="column is-half">
+        <p class="bd-notification is-primary">
+          <input
+            class="input is-rounded"
+            type="text"
+            placeholder="@GitHubAccount here ..."
+          />
+          <button
+            @click="searchAction"
+            id="searchButton"
+            class="button is-rounded"
+          >
+            Search
+          </button>
+        </p>
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <figure class="image is-64x64">
+                <img
+                  :src="profile.ava"
+                  alt="Image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>John Smith</strong> <small>@johnsmith</small>
+                  <small>31m</small>
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aenean efficitur sit amet massa fringilla egestas. Nullam
+                  condimentum luctus turpis.
+                </p>
+              </div>
+              <nav class="level is-mobile">
+                <div class="level-left">
+                  <a class="level-item" aria-label="reply">
+                    <span class="icon is-small">
+                      <i class="fas fa-reply" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="retweet">
+                    <span class="icon is-small">
+                      <i class="fas fa-retweet" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="like">
+                    <span class="icon is-small">
+                      <i class="fas fa-heart" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      input: "",
+      url: `https://api.github.com/users/${this.input}`,
+      profile: {
+        followers: "",
+        following: "",
+        avatar: "",
+        email: "",
+        bio: "",
+      },
+      error: "",
+    };
+  },
+  methods: {
+    searchAction: async function () {
+      await axios
+        .get(this.url)
+        .then((res) => {
+          this.profile.followers = res.data.followers;
+          this.profile.following = res.data.following;
+          this.profile.avatar = res.data.avatar_url;
+          this.profile.email = res.data.email;
+          this.profile.bio = res.data.bio;
+        })
+        .catch((error) => {
+          this.error = error;
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,6 +110,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 350px;
+}
+
+#searchButton {
+  margin-top: 10px;
 }
 </style>
